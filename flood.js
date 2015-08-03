da14a22514d820b33df790ec6141a6e93a2097a6
dorisofsky <dorisofsky@gmail.com>
2015-08-03 09:47:32 +0800

    修改了一堆縮排，也修改了csv成依據dorisofsky不要再依據青恩的

--- flood.js
+++ flood.js
@@ -2,93 +2,89 @@ $(document).ready(function() {
     drawTaiwan()
    });
   
- function drawTaiwan(){
+function drawTaiwan(){
 
-var width = 1024;
-var height = 700;
+  var width = 1024;
+  var height = 700;
 
-var rateById = {};
-var nameById = {};
+  var rateById = {};
+  var nameById = {};
 
-var color_domain = [5, 10, 15, 25, 35]
+  var color_domain = [5, 10, 15, 25, 35]
   var ext_color_domain = [0, 5, 10, 15, 25, 35]
   var legend_labels = ["< 5", "5+", "10+", "15+", "25+", "> 35"]              
-  
 
-var color = d3.scale.threshold()
-  .domain(color_domain)
-  .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
+  var color = d3.scale.threshold()
+    .domain(color_domain)
+    .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
 
+  var div = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
 
-var div = d3.select("body").append("div")   
-  .attr("class", "tooltip")               
-  .style("opacity", 0);
-
- var svg = d3.select("body").append("svg")
+  var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
   .style("margin", "10px auto");
 
- var projection = d3.geo.mercator()
- .center([121,24])
- .scale(8000)
- .translate([width / 2, height / 2]);
+  var projection = d3.geo.mercator()
+  .center([121,24])
+  .scale(8000)
+  .translate([width / 2, height / 2]);
 
- var path = d3.geo.path().projection(projection);
+  var path = d3.geo.path().projection(projection);
 
- d3.csv("http://elsiehsieh.github.io/disastermap/disaster.csv", function(data){
+  d3.csv("http://dorisofsky.github.io/disastermap/disaster.csv", function(data){
+    d3.json("TWN_TOWN_v2_topo.json", function(error, town){
+      for (var i=0; i< data.length; i++){
+        var dataTown = data[i].TOWN_ID;
+        var dataValue = data[i].flood;
 
-  d3.json("TWN_TOWN_v2_topo.json", function(error, town){
-    for (var i=0; i< data.length; i++){
-      var dataTown = data[i].Town_ID;
-      var dataValue = data[i].flood;
+        for(var j=0; j<town.features.properties.length;j++){
+          var jsonTown = town.features[j].properties.TOWN_ID;
 
-      for(var j=0; j<town.features.properties.length;j++){
-        var jsonTown = town.features[j].properties.TOWN_ID;
+          if(dataTown==jsonTown){
+            town.features[j].properties.flood = dataValue
+            break;
+          }
 
-        if(dataTown==jsonTown){
-          town.features[j].properties.flood = dataValue
-          break;
         }
-
       }
-    }
-  
-  var sectortown = topojson.feature(town, town.objects.sector);
+    
+      var sectortown = topojson.feature(town, town.objects.sector);
 
-  svg.append("g")
-    .attr("class", "sector")
-    .selectAll("path")
-    .data(topojson.feature(town, town.features).features)
-    .enter().append("path")
-    .attr("d", path)
-    .style("fill", function(d) {
-     var Flood = d.properties.flood;
-      if (Flood) {
-                   return color(Flood);
-      } else {
-                   return "#ddd";
-    }})
-    .style("opacity", 0.8)
+      svg.append("g")
+        .attr("class", "sector")
+        .selectAll("path")
+        .data(topojson.feature(town, town.features).features)
+        .enter().append("path")
+        .attr("d", path)
+        .style("fill", function(d) {
+           var Flood = d.properties.flood;
+              if (Flood) {return color(Flood);} 
+              else {return "#ddd";}
+            })
+        .style("opacity", 0.8)
 
-    .on("mouseover", function(d) {
-      d3.select(this).transition().duration(300).style("opacity", 1);
-      div.transition().duration(300)
-      .style("opacity", 1)
-      div.text(d.properties.TOWN+ " ─ 洪災數量：" + d.properties.flood) //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY -30) + "px");
-    })
-    .on("mouseout", function() {
-      d3.select(this)
-      .transition().duration(300)
-      .style("opacity", 0.8);
-      div.transition().duration(300)
-      .style("opacity", 0);
-    })
+        .on("mouseover", function(d) {
+          d3.select(this).transition().duration(300).style("opacity", 1);
+          div.transition().duration(300)
+          .style("opacity", 1)
+          div.text(d.properties.TOWN+ " ─ 洪災數量：" + d.properties.flood) //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY -30) + "px");
+        })
 
+        .on("mouseout", function() {
+          d3.select(this)
+          .transition().duration(300)
+          .style("opacity", 0.8);
+          div.transition().duration(300)
+          .style("opacity", 0);
+        })
+
+    });
   });
-});
 
   var legend = svg.selectAll("g.legend")
     .data(ext_color_domain)
@@ -97,8 +93,8 @@ var div = d3.select("body").append("div")
 
   var ls_w = 20, ls_h = 20;
 
-    legend.append("rect")
-      .attr("x", 20)
+  legend.append("rect")
+    .attr("x", 20)
     .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
     .attr("width", ls_w)
     .attr("height", ls_h)
