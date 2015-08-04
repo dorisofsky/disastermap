cb2588f83edac43bcbb6b8d31f5a9219b43ef9c0
dorisofsky <dorisofsky@gmail.com>
2015-08-04 11:22:14 +0800

    發現有些東西似乎是不需要的，隱藏起來

--- flood.js
+++ flood.js
@@ -7,30 +7,30 @@ function drawTaiwan(){
   var width = 1024;
   var height = 700;
 
-  var rateById = {};
-  var nameById = {};
+  // var rateById = {};
+  // var nameById = {};
 
   var color_domain = [5, 10, 15, 25, 35]
   var ext_color_domain = [0, 5, 10, 15, 25, 35]
   var legend_labels = ["< 5", "5+", "10+", "15+", "25+", "> 35"]              
 
   var color = d3.scale.threshold()
-    .domain(color_domain)
-    .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
+  .domain(color_domain)
+  .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
 
   var div = d3.select("body").append("div")   
-    .attr("class", "tooltip")               
-    .style("opacity", 0);
+  .attr("class", "tooltip")               
+  .style("opacity", 0);
 
   var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .style("margin", "10px auto");
+  .attr("width", width)
+  .attr("height", height)
+  .style("margin", "10px auto");
 
   var projection = d3.geo.mercator()
-    .center([121,24])
-    .scale(8000)
-    .translate([width / 2, height / 2]);
+  .center([121,24])
+  .scale(8000)
+  .translate([width / 2, height / 2]);
 
   var path = d3.geo.path().projection(projection);
 
@@ -55,56 +55,57 @@ function drawTaiwan(){
       var sectortown = topojson.feature(town, town.objects.TWN_TOWN_v2);
 
       svg.append("g")
-        .attr("class", "TWN_TOWN_v2")
-        .selectAll("path")
-        .data(topojson.feature(town, town.objects.TWN_TOWN_v2.geometries).features)
-        .enter().append("path")
-        .attr("d", path)
-        .style("fill", function(d) {
-          var Flood = d.flood;
-          if (Flood) {return color(Flood);} 
-                else {return "#ddd";}
-        })
-        .style("opacity", 0.8)
+      .attr("class", "TWN_TOWN_v2")
+      .selectAll("path")
+      .data(topojson.feature(town, town.objects.TWN_TOWN_v2.geometries).features)
+      .enter().append("path")
+      .attr("d", path)
+      .style("fill", function(d) {
+        var Flood = d.flood;
+        if (Flood) {return color(Flood);} 
+              else {return "#ddd";}
+      })
+      .style("opacity", 0.8)
 
-        .on("mouseover", function(d) {
-          d3.select(this).transition().duration(300).style("opacity", 1);
-          div.transition().duration(300)
-          .style("opacity", 1)
-          div.text(d.id+ " ─ 洪災數量：" + d.flood) //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY -30) + "px");
-        })
+      .on("mouseover", function(d) {
+        d3.select(this).transition().duration(300).style("opacity", 1);
+        div.transition().duration(300)
+        .style("opacity", 1)
+        div.text(d.id+ " ─ 洪災數量：" + d.flood) 
+        //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY -30) + "px");
+      })
 
-        .on("mouseout", function() {
-          d3.select(this)
-          .transition().duration(300)
-          .style("opacity", 0.8);
-          div.transition().duration(300)
-          .style("opacity", 0);
-        })
+      .on("mouseout", function() {
+        d3.select(this)
+        .transition().duration(300)
+        .style("opacity", 0.8);
+        div.transition().duration(300)
+        .style("opacity", 0);
+      })
 
     });
   });
 
   var legend = svg.selectAll("g.legend")
-    .data(ext_color_domain)
-    .enter().append("g")
-    .attr("class", "legend");
+  .data(ext_color_domain)
+  .enter().append("g")
+  .attr("class", "legend");
 
   var ls_w = 20, ls_h = 20;
 
   legend.append("rect")
-    .attr("x", 20)
-    .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
-    .attr("width", ls_w)
-    .attr("height", ls_h)
-    .style("fill", function(d, i) { return color(d); })
-    .style("opacity", 0.8);
+  .attr("x", 20)
+  .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+  .attr("width", ls_w)
+  .attr("height", ls_h)
+  .style("fill", function(d, i) { return color(d); })
+  .style("opacity", 0.8);
 
   legend.append("text")
-    .attr("x", 50)
-    .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
-    .text(function(d, i){ return legend_labels[i]; });
+  .attr("x", 50)
+  .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+  .text(function(d, i){ return legend_labels[i]; });
 
 }
